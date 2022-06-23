5986ff0a9c31345671d02b9a3acca959f2b88127
Deepak <aroradeepak485@gmail.com>
2022-06-23 21:46:28 +0530

    Prev and Next buttons

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props
+    let {title, description, urlToImage, newsUrl} = this.props
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
-  <img src={imageUrl} className="card-img-top" alt="..."/>
+        <div className="card my-2" style={{width: "18rem"}}>
+  <img src={urlToImage} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href="/" className="btn btn-sm btn-info">Read More</a>
+    <a href= {newsUrl} target = "_blank" className="btn btn-sm btn-dark">Read More</a>
   </div>
 </div>
       </div>
