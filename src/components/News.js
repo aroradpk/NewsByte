f95d8cd3f12e0010cf21d23f91ebabc6723112a5
Deepak <aroradeepak485@gmail.com>
2022-06-24 21:08:23 +0530

    Router and dynamic headlines

--- src/components/News.js
+++ src/components/News.js
@@ -1,64 +1,74 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
 
 export class News extends Component {
 
-  constructor() {
-    super();
+  static defaultProps = {
+    country : "in",
+    pageSize: "9"
+  }
+
+  static propTypes = {
+    country : PropTypes.string,
+    pageSize : PropTypes.number,
+    category : PropTypes.string
+  }
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page:1,
       
     }
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsByte`;
+  }
+
+  capitalizeFirstLetter = (string) =>{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  async updateNews(){
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6f93c6c49346499a8c4834528b9321c1&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading : true})
+    let data = await fetch(url)
+    let parsedData = await data.json()
+    this.setState({ articles: parsedData.articles, totalResults : parsedData.totalResults, loading : false })
   }
 
   async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=6f93c6c49346499a8c4834528b9321c1&page=1&pageSize=20";
-    let data = await fetch(url)
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({ articles: parsedData.articles, totalResults : parsedData.totalResults })
+    this.updateNews();
   }
 
 prevClickHandler = async() => {
-  let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6f93c6c49346499a8c4834528b9321c1&page=${this.state.page - 1}&pageSize=20`;
-    let data = await fetch(url)
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({
-      page : this.state.page - 1,
-      articles: parsedData.articles })
+  this.setState({page : this.state.page - 1});
+  this.updateNews();
 }
 
 nextClickHandler = async() => {
-  if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/20))){
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6f93c6c49346499a8c4834528b9321c1&page=${this.state.page + 1}&pageSize=20`;
-    let data = await fetch(url)
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({
-      page : this.state.page + 1,
-       articles: parsedData.articles })
-  }
-  
+  this.setState({page : this.state.page + 1});
+  this.updateNews();
 }
 
   render() {
     return (
       <div className='container my-3'>
-        <h1 className="text-center">Newsaholic - Top Headlines</h1>
+        <h1 className="text-center">NewsByte - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+        {this.state.loading && <Spinner />}
         <div className='row my-3'>
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             let { url, title, description, urlToImage } = element
             return <div className='col-md-4' key={url}>
-              <NewsItem title={element.title ? title.slice(0, 46) : ""} description={element.description ? description.slice(0, 88) : ""} urlToImage={element.urlToImage ? urlToImage : "https://www.masscommunicationtalk.com/wp-content/uploads/2011/10/News-handing-665x435.gif"} newsUrl={url} />
+              <NewsItem title={element.title ? title.slice(0, 46) : ""} description={element.description ? description.slice(0, 88) : ""} urlToImage={element.urlToImage ? urlToImage : "https://www.masscommunicationtalk.com/wp-content/uploads/2011/10/News-handing-665x435.gif"} newsUrl={url} author={element.author?element.author:"Anonymous"} date={element.publishedAt} source={element.source.name}/>
             </div>
           })}
         </div>
         <div className="container d-flex justify-content-between">
           <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.prevClickHandler}>&larr; Previous</button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-dark" onClick={this.nextClickHandler}>Next &rarr;</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.nextClickHandler}>Next &rarr;</button>
         </div>
 
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, urlToImage, newsUrl} = this.props
+    let { title, description, urlToImage, newsUrl, author, date, source } = this.props
     return (
       <div>
-        <div className="card my-2" style={{width: "18rem"}}>
-  <img src={urlToImage} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href= {newsUrl} target = "_blank" className="btn btn-sm btn-dark">Read More</a>
-  </div>
-</div>
+        <div className="card my-2" >
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'85%', zIndex: '1'}}>
+                {source}
+              </span>
+          <img src={urlToImage} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted"> By {author} on {new Date(date).toLocaleString("en-US")}</small></p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+          </div>
+        </div>
       </div>
     )
   }
